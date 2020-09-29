4229c4250a3c01b698ea01e994741293f8da6bce
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2020-09-30 00:24:11 +0100

    first init

---
+++ Word Scramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Mohammed Alqumairi on 30/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
